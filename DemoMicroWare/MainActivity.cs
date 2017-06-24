9e2d89cd0d58e117423660b0766d80d98959343a
admin <admin@NESARA>
2017-06-24 14:22:11 +0530

    Add project files.

---
+++ DemoMicroWare/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+
+
+namespace DemoMicroWare
+{
+    [Activity(Label = "DemoMicroWare", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            MobileCenter.Start("dc2ed24c-7212-4ef8-a3de-03811fb8a6e7",
+                   typeof(Analytics), typeof(Crashes));
+
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+        }
+    }
+}
+
